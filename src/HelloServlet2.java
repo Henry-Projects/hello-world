e22d9c62bf5369e712bd9e1c5b9b197560520324
Henry-Projects <henryvu.contact@gmail.com>
2019-04-15 17:56:15 -0700

    test4

--- src/HelloServlet2.java
+++ src/HelloServlet2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 
-//hello2
+//hello4
 
 @WebServlet(urlPatterns = { "/hello" })
         public class HelloServlet2 extends GenericServlet {
